7636f214590c6962d77328708a698a6b6baf2b5b
quyvu <quy.vu.0101@gmail.com>
2026-07-20 21:47:53 +0700

    change visibility of ConsumerAmbient from public to internal.

--- src/FxLink/Statics/ConsumerAmbient.cs
+++ src/FxLink/Statics/ConsumerAmbient.cs
@@ -2,13 +2,13 @@ using FxLink.Entities;
 
 namespace FxLink.Statics;
 
-public static class ConsumerAmbient
+internal static class ConsumerAmbient
 {
     private static readonly AsyncLocal<ConsumerAmbientData> AsyncLocal = new();
 
     internal static void SetConsumerAmbientData(IServiceProvider services, Type consumerType) =>
         AsyncLocal.Value = new ConsumerAmbientData(services, consumerType);
 
-    public static IServiceProvider Services => AsyncLocal.Value.Services;
-    public static Type ConsumerType => AsyncLocal.Value.ConsumerType;
+    internal static IServiceProvider Services => AsyncLocal.Value.Services;
+    internal static Type ConsumerType => AsyncLocal.Value.ConsumerType;
 }
